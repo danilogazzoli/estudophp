e9211895e40540bb4d5f25fbaa4ac34e863d007c
Wellington Torrejais da Silva <wtds.trabalho@gmail.com>
2018-08-23 19:07:16 -0300

    Iterar array já existente $_SERVER
    Uso muito avançado para agora... 
    Tente ler estes conteúdos antes:
    
    https://secure.php.net/manual/en/control-structures.foreach.php
    https://stackoverflow.com/questions/1222244/loop-through-get-results

--- exercicio01.php
+++ exercicio01.php
@@ -16,15 +16,23 @@
 
     $phpInfo = new gazzoli\PHPInfo\PHPInfoParser();
 
-    $arr = $phpInfo->parse_phpinfo();
+    $arr = $phpInfo->parse_phpinfo(); // Muito avançado
+  
+    # Inicie por algo mais simples como 
+    # https://secure.php.net/manual/en/control-structures.foreach.php
 
-    echo '<ol>';
+    # Tal como fez abaixo... 
+    # Só que o array seria um array de valores
+    # E não o resultado desta classe
+    echo '<ol>'; // Dica: https://stackoverflow.com/questions/1222244/loop-through-get-results
+  
+    # Itere $_SERVER ao onvés de $arr
+    # Mas a dica é que a sintaxe do foreach muda...
     foreach ($arr as &$value) {
       echo $value;
       //echo "<li>"$value"</li>"
     }
     echo "</ol>";
-
     ?>
 
 </body>
