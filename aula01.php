94f4d5d19cd4f7660b751f0ba17a471e189081db
Wellington Torrejais da Silva <wtds.trabalho@gmail.com>
2018-08-23 18:51:21 -0300

    Ausência de declaração de variáveis
    Declarar a variável $XXX
    Segue referência:
    https://secure.php.net/manual/en/language.variables.scope.php

--- aula01.php
+++ aula01.php
@@ -3,8 +3,8 @@
   echo '-------------------';
   echo($_SERVER['USER']);
   echo '-------------------';
-  echo $XXX;
-  echo $XXX;
+  echo $XXX; // Só não foi declarada antes
+  echo $XXX; // Neste caso o PHP vai reclamar ao interpretar
   echo $SERVER_PROTOCOL."<br>";
   //print_r($_SERVER)
 
